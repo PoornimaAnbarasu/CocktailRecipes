b606d9c95e51053042add3c230c6e5f7e6661fea
poornima anbarasu <poornima.anbarasu@gmail.com>
2021-03-23 15:09:17 -0400

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../logo.svg';
+import './Navbar.css';
+export default function Navbar() {
+  return (
+    <nav className='navbar'>
+      <div className='nav-center'>
+        <Link to='/'>
+          {/* <h1>The Cocktail Database</h1> */}
+          <img src={logo} alt='cocktail db logo' className='logo' />
+        </Link>
+        <ul className='nav-links'>
+          <li>
+            <Link to='/'>home</Link>
+          </li>
+          <li>
+            <Link to='/about'>about</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        One can refer this website to know in depth about the different
+        cocktails available in the market. This is a cocktail database which
+        list the details of each Cocktail. It gives the name of the cocktail
+        chosen and its category.It also gives information whether its alcoholic
+        or non-alcoholic. It also lets know what kind of glass best suite this
+        cocktail. It also let know the instruction of to how to. Last but not
+        least the info section of the ingrediets used.
+      </p>
+    </section>
+  );
+}
